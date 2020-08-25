b09986e27bc283cee65acb8f7f45df812100c14f
Spenser Black <spenserblack01@gmail.com>
2020-08-25 16:09:55 -0400

    Style App

--- src/App.vue
+++ src/App.vue
@@ -59,6 +59,7 @@ export default {
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   text-align: center
-  color: #2c3e50
-  margin-top: 60px
+  background-color: #222
+  color: YellowGreen
+  height: 100vh
 </style>
