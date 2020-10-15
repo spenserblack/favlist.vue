2478dcc17709901de1fc0a8341b1e16a868ffac3
Spenser Black <spenserblack01@gmail.com>
2020-10-15 09:31:31 -0400

    Animate alert exit

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,5 +1,6 @@
 <template lang='pug'>
-  p.alert #[slot]
+  transition(name='alert')
+    p.alert #[slot]
 </template>
 
 <script>
@@ -7,3 +8,15 @@ export default {
   name: 'Alert',
 };
 </script>
+
+<style lang='stylus' scoped>
+.alert
+  position: relative
+
+.alert-leave-active
+  transition: all .75s ease
+
+.alert-leave-to
+  opacity: 0
+  transform: translateY(-5em)
+</style>
