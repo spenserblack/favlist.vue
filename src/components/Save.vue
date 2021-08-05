d5a915d770f159992daff758bf01dc235a91bb08
Spenser Black <spenserblack01@gmail.com>
2021-08-05 07:57:39 -0400

    Use carbon icons for copy/import/export
    Provides more consistency between browsers, because the carbon icons use
    paths instead of text, so there are no font issues.
    
    Instead of setting SVG width and height, width and height is set using
    CSS for wrapping elements. A new component was created to standardize
    this.
    
    Fixes #83

--- src/components/Save.vue
+++ src/components/Save.vue
@@ -1,31 +1,21 @@
 <template lang='pug'>
-  button.flipped(@click='saveFavlists' title='Save as JSON')
-    JsonSaveSvg(
-      :width='saveJsonWidth'
-      :height='saveJsonHeight'
-      name='save JSON'
-    )
+  button(@click='saveFavlists' title='Save as JSON')
+    Icon: JsonIcon
+    Icon: DownloadIcon
 </template>
 
 <script>
-import JsonSaveSvg from './svg/JsonImport.vue';
+import {
+  DocumentDownload as DownloadIcon,
+  Json as JsonIcon,
+} from '@v2icons/carbon';
+import Icon from './Icon.vue';
 import {saveAs} from 'file-saver';
 import stringifyJson from 'core-js/stable/json/stringify';
 
-const saveJsonWidth = 36;
-const saveJsonHeight = 36;
-
 export default {
   name: 'Save',
-  components: {
-    JsonSaveSvg,
-  },
-  data() {
-    return {
-      saveJsonWidth,
-      saveJsonHeight,
-    };
-  },
+  components: { DownloadIcon, JsonIcon, Icon },
   methods: {
     saveFavlists() {
       const blob = new Blob(
@@ -37,8 +27,3 @@ export default {
   },
 };
 </script>
-
-<style lang='stylus' scoped>
-.flipped
-  transform: scaleY(-1)
-</style>
