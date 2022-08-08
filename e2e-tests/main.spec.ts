896abe99694f1b13bc96b08c6eb9154198ba0416
Spenser Black <spenserblack01@gmail.com>
2022-08-08 18:29:48 -0400

    Test window title

--- e2e-tests/main.spec.ts
+++ e2e-tests/main.spec.ts
@@ -1,21 +1,13 @@
 import { resolve } from 'path';
 import { _electron as electron } from 'playwright';
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('placeholder test', async () => {
   const electronApp = await electron.launch({ args: [resolve(__dirname, '../build-test/main.js')] });
 
-  const appPath = await electronApp.evaluate(({ app }) => app.getAppPath());
-  console.log(appPath);
-
   const window = await electronApp.firstWindow();
 
-  console.log(await window.title());
-
-  // Redirect console.log to the terminal.
-  window.on('console', console.log);
-
-  await window.click('input[type=text]');
+  expect(await window.title()).toBe('Favlist');
 
   await electronApp.close();
 });
