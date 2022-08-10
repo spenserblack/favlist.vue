b6b660b01e67e47febe403852ae1af10f5ece407
Spenser Black <spenserblack01@gmail.com>
2022-08-10 11:47:09 -0400

    Allow build to be skipped with environment variable

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,16 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const port = process.env.PORT || '4173';
+const webServerCommands = [
+  ...(!process.env.FAVLIST_TEST_NO_BUILD ? ['yarn build:dev'] : []),
+  `yarn serve:renderer --port ${port}`,
+].join(' && ');
 
 const config: PlaywrightTestConfig = {
   testDir: './e2e-tests',
   maxFailures: 2,
   webServer: {
-    command: `yarn build:dev && yarn serve:renderer --port ${port}`,
+    command: webServerCommands,
     port: Number.parseInt(port, 10),
   },
 };
