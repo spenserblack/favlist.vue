3556099dd274342bea03c8f8113c4f6dd4275f99
Spenser Black <spenserblack01@gmail.com>
2022-09-19 15:53:41 -0400

    Allow dark mode to be toggled
    Instead of forcing users into using dark mode, makes dark mode a value
    that can be toggled on and off. Saves to localStorage.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import {
+  QBtn,
+
   QLayout,
   QHeader,
   QFooter,
@@ -18,9 +20,22 @@ import {
   useQuasar,
 } from 'quasar';
 import { setWindowTitle } from './electron';
+import { load as loadDarkMode, save as saveDarkMode } from './theme';
 
 const $q = useQuasar();
-$q.dark.set(true);
+
+$q.dark.set(loadDarkMode());
+const isDark = computed({
+  get: () => $q.dark.isActive,
+  set: (value: boolean) => {
+    $q.dark.set(value);
+    saveDarkMode(value);
+  },
+});
+const toggleDark = () => {
+  isDark.value = !isDark.value;
+};
+const toggleDarkIcon = computed(() => `sym_o_${isDark.value ? 'light_mode' : 'dark_mode'}`);
 
 const title = ref('Favlist');
 const onTitleUpdate = (favlistTitle: string | null) => {
@@ -45,6 +60,13 @@ const onTitleUpdate = (favlistTitle: string | null) => {
         <QTabs shrink>
           <QRouteTab :to="{ name: 'home' }" icon="sym_o_home">Home</QRouteTab>
         </QTabs>
+        <QBtn
+            round
+            dense
+            flat
+            :icon="toggleDarkIcon"
+            @click="toggleDark"
+        />
       </QToolbar>
     </QHeader>
     <QPageContainer>
---
+++ src/renderer/theme.ts
@@ -0,0 +1,20 @@
+const themeKey = 'favlist--dark-theme';
+
+/**
+ * @return True for dark mode, false for light mode, 'auto' to pick automatically
+ */
+export function load(): boolean | 'auto' {
+  const value = window?.localStorage?.getItem(themeKey);
+  switch (value) {
+    case 'true':
+      return true;
+    case 'false':
+      return false;
+    default:
+      return 'auto';
+  }
+}
+
+export function save(darkMode: boolean): void {
+  window?.localStorage?.setItem(themeKey, darkMode.toString());
+}
