205b7afc58d67da1ff8b8008a4a25a6d1ee5c520
Spenser Black <spenserblack01@gmail.com>
2022-11-28 09:15:32 -0500

    Increase default timeout for browser context
    This includes events like the 'window' event, which is required to get
    the first window.

--- e2e-tests/setup.ts
+++ e2e-tests/setup.ts
@@ -8,6 +8,8 @@ export default async function setup(): Promise<[ElectronApplication, Page]> {
   const app = await electron.launch({
     args: [resolve(__dirname, "../build/main/main.js")],
   });
+  const context = app.context();
+  context.setDefaultTimeout(60000);
   const page = await app.firstWindow();
   return [app, page];
 }
